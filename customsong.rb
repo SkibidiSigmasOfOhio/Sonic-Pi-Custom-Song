894ef038c04408848aa6f64e29ca7dba6a81c5a8
SkibidiSigmasOfOhio <alberto_sanchez@student.davincischools.org>
2025-02-27 11:32:43 -0800

    Create customsong.rb

---
+++ customsong.rb
@@ -0,0 +1,58 @@
+use_bpm 124
+use_synth :piano
+
+choir = sample
+
+live_loop :bass do
+  6.times do
+    play :fs3
+    play :d4
+    sleep 1
+    play :fs3
+    play :d4
+    sleep 1
+    play :fs3
+    play :d4
+    sleep 1
+    play :fs3
+    play :d4
+    sleep 1
+  end
+  stop
+end
+
+sleep 8
+
+
+live_loop :anthem do
+  4.times do
+    play :d4
+    play :fs4
+    sleep 0.5
+    play :fs4
+    play :a4
+    sleep 0.5
+    play :fs4
+    play :a4
+    sleep 0.5
+    play :a4
+    play :d5
+    sleep 0.5
+    play :a4
+    play :d5
+    sleep 0.5
+    play :d5
+    play :fs5
+    sleep 0.5
+    play :d5
+    play :fs5
+    sleep 0.5
+    play :fs5
+    play :a5
+    sleep 0.5
+  end
+  sample :ambi_dark_woosh
+  stop
+end
+sleep 16.5
+sample "C:/Users/alberto_sanchez/Downloads/choir.wav"
